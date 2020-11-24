6c8e0f5a76d7a7da2f9d03645f2ac18b47e7973f
rdvetromilla <44167963+rdvetromilla@users.noreply.github.com>
2020-11-24 15:38:11 -0300

    Fix Order Entity constant typo (#1)

--- src/Getnet/API/Order.php
+++ src/Getnet/API/Order.php
@@ -13,7 +13,7 @@ class Order implements \JsonSerializable {
     const PRODUCT_TYPE_DIGITAL_GOODS    = "digital_goods";
     const PRODUCT_TYPE_DIGITAL_PHYSICAL = "digital_physical";
     const PRODUCT_TYPE_GIFT_CARD        = "gift_card";
-    const PRODUCT_TYPE_PHISICAL_GOODS   = "phisical_goods";
+    const PRODUCT_TYPE_PHISICAL_GOODS   = "physical_goods";
     const PRODUCT_TYPE_RENEW_SUBS       = "renew_subs";
     const PRODUCT_TYPE_SHAREWARE        = "shareware";
     const PRODUCT_TYPE_SERVICE          = "service";
